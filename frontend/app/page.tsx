be68227718339db15022d6155e0c520a7534f488
stephane agoume <agoumekoufanas@gmail.com>
2025-07-27 20:46:55 -0400

    implement cicd

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Bienvenue sur OlaboPhoto Gamification !</h1>
-      <p className="text-lg mt-4">Le jeu "Devinez le contexte de la photo" arrive bientôt. :) </p>
+      <p className="text-lg mt-4">Le jeu "Devinez le contexte de la photo" arrive bientôt. - :) </p>
       <div className="mt-8">
         <p>Veuillez vous connecter ou vous inscrire.</p>
         {/* Ici, tu ajouteras tes composants de connexion/inscription */}
